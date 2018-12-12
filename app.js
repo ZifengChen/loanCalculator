cdb54116f8632afe6b6c7fba1790fdf38f389eba
ZifengChen <ken36chan@gmail.com>
2018-12-12 01:55:44 -0500

    1:55

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 //Listen for submit
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    //Hide Results
+    document.getElementById('results').style.display = 'none';
+    //Show Loader
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+    e.preventDefault()
+});
 
 //Calculate Results
-function calculateResults(e){
+function calculateResults() {
     console.log('Calculating..');
     //UI vars
     const amount = document.getElementById('amount');
@@ -18,41 +26,52 @@ function calculateResults(e){
 
     //compute monthly payment
     const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-    const monthly = (principal*x*calculatedInterest)/(x-1);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
 
-    if(isFinite(monthly)) {
+    if (isFinite(monthly)) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
-      } else {
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        document.getElementById('results').style.display = 'block';
+
+        document.getElementById('loading').style.display = 'none';
+    } else {
         showError('Please check your numbers');
-      }
-      e.preventDefault();
     }
-    
-    // Show Error
-    function showError(error){
-      // Create a div
-      const errorDiv = document.createElement('div');
-    
-      // Get elements
-      const card = document.querySelector('.card');
-      const heading = document.querySelector('.heading');
-    
-      // Add class
-      errorDiv.className = 'alert alert-danger';
-    
-      // Create text node and append to div
-      errorDiv.appendChild(document.createTextNode(error));
-    
-      // Insert error above heading
-      card.insertBefore(errorDiv, heading);
-    
-      // Clear error after 3 seconds
-      setTimeout(clearError, 3000);
-    }
-    
-    // Clear error
-    function clearError(){
-      document.querySelector('.alert').remove();
-    }
+}
+// Show Error
+function showError(error) {
+
+    //Hide results
+    document.getElementById('results').style.display = 'none';
+
+    //Hide loader
+    document.getElementById('loading').style.display = 'none';
+
+    // Create a div
+    const errorDiv = document.createElement('div');
+
+    // Get elements
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    // Add class
+    errorDiv.className = 'alert alert-danger';
+
+    // Create text node and append to div
+    errorDiv.appendChild(document.createTextNode(error));
+
+    // Insert error above heading
+    card.insertBefore(errorDiv, heading);
+
+    // Clear error after 3 seconds
+    setTimeout(clearError, 3000);
+
+
+}
+
+// Clear error
+function clearError() {
+    document.querySelector('.alert').remove();
+}
